21c6de18ac6574075b55a6fdb79ebe3efe46d717
ramandevops789 <ramana.linux789@gmail.com>
2024-12-03 22:47:03 +0530

    colors

---
+++ 13-colors.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne o ]
+    then
+        echo "Please log in with root privilages"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is ...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$1 is ...$G SUCCES $N"
+    fi
+}
+
+CHECK_ROOT
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to installed it"
+    dnf install git -y
+    VALIDATE $? "Insstalling Git"
+else
+    echo "Git is already installed, nothing to do"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "Mysql is not installed, going to install"
+    dnf install mysql -y
+    VALIDATE $? "Installing Mysql"
+else
+    echo "Mysql is already installed, nothing to do"
+fi
