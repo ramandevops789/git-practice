727f3bfc299885ed9cf4b5a32e9089e89ef4c4c6
ramandevops789 <ramana.linux789@gmail.com>
2024-12-05 01:02:47 +0530

    deleting the old logs

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SOURCE_DIR=/home/ec2-user/logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIR ]
+then
+    echo -e "$SOURCE_DIR $G Exists $N"
+else
+    echo -e "$SOURCE_DIR $R does not exist $N"
+    exit 1
+fi
+
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+echo "Files: $FILES"
+
+while IFS=read -r file
+do
+    echo "Deleting file: $file"
+    rm -rf $file
+    done <<< $FILES
