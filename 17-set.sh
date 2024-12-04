ff84945377c019ab6f005819e7da803796a6d378
ramandevops789 <ramana.linux789@gmail.com>
2024-12-05 00:52:39 +0530

    set-trap

---
+++ 17-set.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e #Setting the automatic exit, if we get error set -ex for debug.
+
+failure(){
+    echo "Failed at: $1:$2"
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR #ERR is the error signal
+
+echo "Hello world success"
+echooooo "Hello world failure"
+echo "Hello world after failure"
