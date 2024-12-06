c6b1299a40fa8cb91418f819718273f063e4157d
ramandevops789 <ramana.linux789@gmail.com>
2024-12-06 19:06:27 +0530

    redirectors

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -26,7 +26,7 @@ VALIDATE(){
         echo -e "$2 is ... $R FAILED $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$1 is ...$G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 is ...$G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
